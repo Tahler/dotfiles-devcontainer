4ff13caf04e1912269c0dac3b3e79a0589408863
Tyler Berry <tjberry@verily.com>
2023-05-19 23:43:07 -0700

    redo install

--- install.sh
+++ install.sh
@@ -2,11 +2,22 @@
 
 DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit ; pwd -P )
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+OH_MY_ZSH_PATH="$HOME/.oh-my-zsh"
+if [[ ! -d "$OH_MY_ZSH_PATH" ]]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
-rm -f "$HOME/.zshrc"
-ln -sf "$DIR/.zshrc" "$HOME/.zshrc"
+POWERLEVEL10K_PATH="${ZSH_CUSTOM:-$OH_MY_ZSH_PATH/custom}/themes/powerlevel10k"
+if [[ ! -d "$POWERLEVEL10K_PATH" ]]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$POWERLEVEL10K_PATH"
+fi
 
-git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf"
-"$HOME/.fzf/install" --key-bindings --completion --update-rc
+ZSHRC_PATH="$HOME/.zshrc"
+rm -f "$ZSHRC_PATH"
+ln -sf "$DIR/.zshrc" "$ZSHRC_PATH"
+
+FZF_PATH="$HOME/.fzf"
+if [[ ! -d "$FZF_PATH" ]]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git "$FZF_PATH"
+  "$FZF_PATH/install" --key-bindings --completion --update-rc
+fi
